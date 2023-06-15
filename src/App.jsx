4519afd9487ac6a097bd76f05b43177d322078a7
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-06-15 13:49:02 -0500

    fix deployment stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
-import CardData from './components/CardData'
-import CardDetails from './components/CardDetails'
-import Header from './components/Header'
-import NotFound from './components/NotFound'
-
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import CardData from "./components/CardData";
+import CardDetails from "./components/CardDetails";
+import Header from "./components/Header";
+import NotFound from "./components/NotFound";
 
 function App() {
-
   return (
     <>
       <Header
-        title='PokeStop'
-        color='var(--background-color-light)'
-        navigation={['Home', 'About', 'Contact']}
+        title="PokeStop"
+        color="var(--background-color-light)"
+        navigation={["Home", "About", "Contact"]}
       />
 
       <Routes>
         <Route path="/" element={<CardData pageSize={10} page={1} />} />
+        <Route path="/poke-stop/" element={<CardData pageSize={10} page={1} />} />
         <Route path="/card/:id" element={<CardDetails />} />
-        <Route path="/404" element={<NotFound /> } />
+        <Route path="/404" element={<NotFound />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,33 @@
-import PropTypes from 'prop-types';
-import { useNavigate } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { useNavigate } from "react-router-dom";
 
 const Header = ({ title, color, navigation }) => {
-	const navigate = useNavigate();
+  const navigate = useNavigate();
 
-	const handleTitleClick = () => {
-		navigate('/');
-	};
+  const handleTitleClick = () => {
+    navigate("/");
+  };
 
-	return (
-		<header className='app-header' style={{ backgroundColor: color }}>
-			<div className='container header-content' style={{display: 'flex'}}>
-				<h3 onClick={handleTitleClick}>{title}</h3>
-				<nav>
-					<ul style={{ listStyleType: 'none', display: 'flex', gap: '10px' }}>
-						{navigation.map((item, index) => (
-							<li key={index}>{item}</li>
-						))}
-					</ul>
-				</nav>
-
-			</div>
-		</header>
-	);
+  return (
+    <header className="app-header" style={{ backgroundColor: color }}>
+      <div className="container header-content" style={{ display: "flex" }}>
+        <h3 onClick={handleTitleClick}>{title}</h3>
+        <nav>
+          <ul style={{ listStyleType: "none", display: "flex", gap: "10px" }}>
+            {navigation.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 Header.propTypes = {
-	title: PropTypes.string.isRequired,
-	color: PropTypes.string.isRequired,
-	navigation: PropTypes.arrayOf(PropTypes.string).isRequired,
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  navigation: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Header;
